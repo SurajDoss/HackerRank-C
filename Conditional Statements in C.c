4b501263a2698c26bfc31446b0b66fca5a933161
Suraj Doss <suraj_sebastien@yahoo.com>
2021-02-17 11:36:03 +0530

    Create Conditional Statements in C.c

---
+++ Conditional Statements in C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    static const char *strings[] = {"one","two", "three", "four", "five", "six", "seven",                                    "eight", "nine"};
+    int num = 0;
+    
+    if(scanf("%d", &num)<1){
+        return 1;
+    }
+    if(num>0 && num <10){
+        printf("%s",strings[num-1]);
+    }else if(num > 9){
+        printf("Greater than 9");
+    }
+    return 0;
+}
